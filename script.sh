efd971fb47e37aa5a77eef98fd49c7c331ad0844
Vishwachetan <Vishwachetan.gangashetty5@gmail.com>
2023-06-08 19:22:07 +0530

    Updated script by develop2

--- script.sh
+++ script.sh
@@ -4,6 +4,8 @@
 if [[ `expr $1 % 2` -eq 0 ]] ; then
  echo "$1 is even number"
 else
- echo "$1 is odd number"
+ echo "$1 is odd number, Enter even please"
 fi
+
+echo "Checking merge conflict by develop2"
 
